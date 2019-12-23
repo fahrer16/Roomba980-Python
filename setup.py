3d23a75b492eeeb197e785c4234818c5e9fb5841
fahrer16 <fahrer@gmail.com>
2019-12-23 17:38:48 -0500

    1.2.4

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='Roomba980-Python',
-    version='1.2.3',
+    version='1.2.4',
     license='MIT',
     description='Python program and library to control iRobot Roomba 980 ' \
-    'Vacuum Cleaner',
-    author_email='nick.waterton@med.ge.com',
-    url='https://github.com/NickWaterton/Roomba980-Python',
+    'Vacuum Cleaner for UDI ISY. Forked from project from Nick Waterton to ensure compatability is maintained',
+    author_email='fahrer@gmail.com',
+    url='https://github.com/fahrer16/Roomba980-Python',
     packages=find_packages(),
     install_requires=['paho-mqtt', 'six'],
     include_package_data=True,
